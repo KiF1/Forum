d7082c786ac863ac62c34b3758f0fd8f5ee34ca5
abner <abner.kif1@gmail.com>
2024-02-02 21:02:42 -0300

    conection cloudflare

--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -13,6 +13,13 @@ import { AnswerQuestionUseCase } from "@/domain/forum/application/use-cases/answ
 import { EditAnswerUseCase } from "@/domain/forum/application/use-cases/edit-answer";
 import { DeleteAnswerUseCase } from "@/domain/forum/application/use-cases/delete-answer";
 import { FetchQuestionAnswersUseCase } from "@/domain/forum/application/use-cases/fetch-question-answers";
+import { CommentOnQuestionUseCase } from "@/domain/forum/application/use-cases/comment-on-question";
+import { ChooseQuestionBestAnswerUseCase } from "@/domain/forum/application/use-cases/choose-question-best-answer";
+import { DeleteQuestionCommentUseCase } from "@/domain/forum/application/use-cases/delete-question-comment";
+import { CommentOnAnswerUseCase } from "@/domain/forum/application/use-cases/comment-on-answer";
+import { DeleteAnswerCommentUseCase } from "@/domain/forum/application/use-cases/delete-answer-comment";
+import { FetchQuestionCommentsUseCase } from "@/domain/forum/application/use-cases/fetch-question-comments";
+import { FetchAnswerCommentsUseCase } from "@/domain/forum/application/use-cases/fetch-answer-comments";
 
 import { AuthenticateController } from "./controllers/authenticate.controller";
 import { CreateAccountController } from "./controllers/create-account.controller";
@@ -25,20 +32,14 @@ import { AnswerQuestionController } from "./controllers/answer-question.controll
 import { EditAnswerController } from "./controllers/edit-answer.controller";
 import { DeleteAnswerController } from "./controllers/delete-answer.controller";
 import { FetchQuestionAnswersController } from "./controllers/fetch-question-answers.controller";
-import { ChooseQuestionBestAnswerUseCase } from "@/domain/forum/application/use-cases/choose-question-best-answer";
 import { CommentOnQuestionController } from "./controllers/comment-on-question.controller";
-import { CommentOnQuestionUseCase } from "@/domain/forum/application/use-cases/comment-on-question";
 import { ChooseQuestionBestAnswerController } from "./controllers/choose-question-best-answer.controller";
 import { DeleteQuestionCommentController } from "./controllers/delete-question-comment.controller";
-import { DeleteQuestionCommentUseCase } from "@/domain/forum/application/use-cases/delete-question-comment";
 import { CommentOnAnswerController } from "./controllers/comment-on-answer.controller";
-import { CommentOnAnswerUseCase } from "@/domain/forum/application/use-cases/comment-on-answer";
 import { DeleteAnswerCommentController } from "./controllers/delete-answer-comment.controller";
-import { DeleteAnswerCommentUseCase } from "@/domain/forum/application/use-cases/delete-answer-comment";
 import { FetchQuestionCommentsController } from "./controllers/fetch-question-comments.controller";
-import { FetchQuestionCommentsUseCase } from "@/domain/forum/application/use-cases/fetch-question-comments";
 import { FetchAnswerCommentsController } from "./controllers/fetch-answer-comments.controller";
-import { FetchAnswerCommentsUseCase } from "@/domain/forum/application/use-cases/fetch-answer-comments";
+import { UpploadAttachmentsController } from "./controllers/uppload-attachment.controller";
 
 
 @Module({
@@ -61,7 +62,8 @@ import { FetchAnswerCommentsUseCase } from "@/domain/forum/application/use-cases
     CommentOnAnswerController,
     DeleteAnswerCommentController,
     FetchQuestionCommentsController,
-    FetchAnswerCommentsController
+    FetchAnswerCommentsController,
+    UpploadAttachmentsController
   ],
   providers: [
     CreateQuestionUseCase, 
